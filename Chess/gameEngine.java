af06793a522d9f008f5953c6b6babcf1cf1a58d0
mohamed abdelhamid <hhoms8978@gmail.com>
2023-05-01 22:45:27 +0300

    set piece icon

--- Chess/gameEngine.java
+++ Chess/gameEngine.java
@@ -31,9 +31,34 @@ public class gameEngine extends JFrame {
                 } else {
                     squares[i][j].setBackground(Color.BLACK);
                 }
-                squares[i][j].piece = new Rook(Color.black, j, i);
                 playingBoard.add(squares[i][j]);
-                squares[i][j].piece=new Rook(Color.white,j,i);
+               if(i==1)
+                   squares[i][j].piece=new Pawn(Color.white,j,i);
+               else if (i==6) {
+                   squares[i][j].piece=new Pawn(Color.black,j,i);
+               }
+               else if(i==0&&(j==0||j==7))
+                   squares[i][j].piece=new Rook(Color.white,i,j);
+               else if(i==0&&(j==1||j==6))
+                   squares[i][j].piece=new Knight(Color.white,i,j);
+               else if(i==0&&(j==2||j==5))
+                   squares[i][j].piece=new Bishop(Color.white,i,j);
+               else if(i==0&&j==3)
+                   squares[i][j].piece=new King(Color.white,i,j);
+               else if(i==0&&j==4)
+                   squares[i][j].piece=new Queen(Color.white,i,j);
+                   //Set the second half
+               else if(i==7&&(j==0||j==7))
+                   squares[i][j].piece=new Rook(Color.black,i,j);
+               else if(i==7&&(j==1||j==6))
+                   squares[i][j].piece=new Knight(Color.black,i,j);
+               else if(i==7&&(j==2||j==5))
+                   squares[i][j].piece=new Bishop(Color.black,i,j);
+               else if(i==7&&j==3)
+                   squares[i][j].piece=new King(Color.black,i,j);
+               else if(i==7&&j==4)
+                   squares[i][j].piece=new Queen(Color.black,i,j);
+
             }
             }
 
